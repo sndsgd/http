3e1e7ceaffcf192b08f80e6f0f5a00db4058e727
Russell <r@snds.gd>
2015-05-29 14:00:33 -0400

    adds 204 status code and fixes status text for 203

--- src/Code.php
+++ src/Code.php
@@ -20,7 +20,8 @@ class Code
       200 => "OK",
       201 => "Created",
       202 => "Accepted",
-      203 => "No Content",
+      203 => 'Non-Authoritative Information',
+      204 => 'No Content',
       205 => "Reset Content",
       206 => "Partial Content",
       # redirection
