d5a3a9b69bd1d9d1c2433adcd70b6262f54dcc6d
Russell <r@snds.gd>
2015-09-22 20:57:21 -0400

    adds EncoderAbstract and JsonEncoder

---
+++ src/data/EncoderAbstract.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace sndsgd\http\data;
+
+use \Exception;
+use \sndsgd\DataTrait;
+use \sndsgd\ErrorTrait;
+
+
+/**
+ * The base class for request data encoders
+ */
+abstract class EncoderAbstract
+{
+   use DataTrait, ErrorTrait;
+
+   /**
+    * When the data has been encoded, it'll be stashed here
+    *
+    * @var string
+    */
+   protected $encodedData;
+
+   /**
+    * Encode data to a string
+    *
+    * @return boolean Indicates if the operation was successfull
+    */
+   abstract public function encode();
+
+   /**
+    * @return string
+    */
+   public function getEncodedData()
+   {
+      if (
+         $this->encodedData === null &&
+         !$this->encode()
+      ) {
+         throw new Exception("failed to encode data; ".$this->getError());
+      }
+      return $this->encodedData;
+   }
+}
